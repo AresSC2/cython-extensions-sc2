43f9d3f095738ebec1ee801afb9affc62aa84345
Volker Schmidt <volker.schmidt91@gmail.com>
2025-03-21 22:57:23 +0100

    fix: refactoring

--- cython_extensions/dijkstra.pyi
+++ cython_extensions/dijkstra.pyi
@@ -2,46 +2,40 @@ import numpy as np
 
 
 class DijkstraOutput:
-    """Result of Dijkstras algorithm containing distance and predecessor pointers per cell.
+    """Result of Dijkstras algorithm containing distance and forward pointer grids.
 
     """
-    prev_x: np.ndarray
-    prev_y: np.ndarray
-    dist: np.ndarray
+    forward_x: np.ndarray
+    """Forward pointer grid (x-coordinates)."""
+    forward_y: np.ndarray
+    """Forward pointer grid (y-coordinates)."""
+    distance: np.ndarray
+    """Distance grid."""
 
-    def get_path(self, source: tuple[int, int], limit: int) -> list[tuple[int, int]]:
-        """Follow the path from a given source using the precalculated pointer arrays.
-        
-        Parameters
-        ----------
-        source :
-            The start point of the path.
-        limit :
-            Maximum returned path length. Reduce for a small speedup.
-            
-        Returns
-        -------
-        list[tuple[int, int]] :
+    def get_path(self, source: tuple[int, int], limit: int = 0) -> list[tuple[int, int]]:
+        """Follow the path from a given source using the forward pointer grids.
+
+        Args:
+            source: Start point.
+            limit: Maximum length of the returned path. Defaults to 0 indicating no limit.
+
+        Returns:
             The lowest cost path from source to any of the targets.
             
         """
         ...
 
 
-def cy_dijkstra(cost: np.ndarray, targets: np.ndarray) -> DijkstraOutput:
-    """Run Dijkstras algorithm on a grid, precalulating paths from all points to one of the targets.
+def cy_dijkstra(cost_grid: np.ndarray, targets: np.ndarray, checks_enabled: bool = True) -> DijkstraOutput:
+    """Run Dijkstras algorithm on a grid, yielding many-target-shortest paths for each position.
     
-    Parameters
-    ----------
-    cost :
-        Cost array of shape (n, m). Entries must be positive. Use infinity to mark unpathable cells.
-    targets :
-        Target array of shape (k, 2) containing x and y coordinates of the target points.
+    Args:
+        cost_grid: Cost grid. Entries must be positive. Use infinity to mark unpathable cells.
+        targets: Target array of shape (*, 2) containing x and y coordinates of the K target points.
+        checks_enabled: Pass False to deactivate grid value and target coordinates checks. Defaults to True.
         
-    Returns
-    -------
-    DijkstraOutput :
-        Pathfinding object containing pointer arrays used to construct paths.
+    Returns:
+        Pathfinding object containing distances and pointer grids.
         
     """
     ...
--- cython_extensions/dijkstra.pyx
+++ cython_extensions/dijkstra.pyx
@@ -3,52 +3,58 @@
 from cython import boundscheck, wraparound
 import numpy as np
 
-cimport numpy as cnp
 from libcpp cimport bool
 from libcpp.pair cimport pair
 
-ctypedef cnp.float64_t DTYPE_t
+ctypedef pair[double, pair[Py_ssize_t, Py_ssize_t]] Item
+
+cdef Py_ssize_t[8] NEIGHBOURS_X = [-1, 1, 0, 0, -1, 1, -1, 1]
+cdef Py_ssize_t[8] NEIGHBOURS_Y = [0, 0, -1, 1, -1, -1, 1, 1]
+cdef double SQRT2 = np.sqrt(2)
+cdef double[8] NEIGHBOURS_D = [1, 1, 1, 1, SQRT2, SQRT2, SQRT2, SQRT2]
 
 
 cdef extern from "dijkstra_priority_queue.hpp":
     cdef cppclass cpp_pq:
         cpp_pq(...) except +
-        void push(pair[double,pair[int, int]])
-        pair[double,pair[int, int]] top()
+        void push(Item)
+        Item top()
         void pop()
         bool empty()
 
 
-cdef bool compare_element(pair[double,pair[int, int]] a, pair[double,pair[int, int]] b):
+cdef bool compare_element(Item a, Item b):
     return a.first > b.first
 
 
 cdef class DijkstraOutput:
-    cdef public Py_ssize_t[:, :] prev_x
-    cdef public Py_ssize_t[:, :] prev_y
-    cdef public DTYPE_t[:, :] dist
+    cdef public Py_ssize_t[:, :] forward_x
+    """Forward pointer grid (x-coordinates)."""
+    cdef public Py_ssize_t[:, :] forward_y
+    """Forward pointer grid (y-coordinates)."""
+    cdef public double[:, :] distance
+    """Distance grid."""
     def __cinit__(self,
-                  Py_ssize_t[:, :] prev_x,
-                  Py_ssize_t[:, :] prev_y,
-                  DTYPE_t[:, :] dist,
-                  ):
-        self.prev_x = prev_x
-        self.prev_y = prev_y
-        self.dist = dist
+                  Py_ssize_t[:, :] forward_x,
+                  Py_ssize_t[:, :] forward_y,
+                  double[:, :] distance):
+        self.forward_x = forward_x
+        self.forward_y = forward_y
+        self.distance = distance
 
     @boundscheck(False)
     @wraparound(False)
-    cpdef get_path(self,
-                 (int, int) source,
-                 int limit=2**20) except *:
-        """Follow the path from a given source using the precalculated pointer arrays.
+    cpdef get_path(self, (int, int) source, int limit=0):
+        """
+        
+        Follow the path from a given source using the forward pointer grids.
         
         Parameters
         ----------
         source :
-            The start point of the path.
+            Start point.
         limit :
-            Maximum returned path length. Reduce for a small speedup.
+            Maximum length of the returned path. Defaults to 0 indicating no limit.
             
         Returns
         -------
@@ -58,67 +64,75 @@ cdef class DijkstraOutput:
         """
         path = list[tuple[int, int]]()
         x, y = source
+        if limit == 0:
+            # set a fallback limit to be safe
+            # a path longer than this must contain a cycle, so it should never be hit anyway
+            limit = self.distance.shape[0] * self.distance.shape[1]
+
         while len(path) < limit:
             if x < 0 or y < 0:
                 break
             path.append((x, y))
-            x, y = self.prev_x[x, y], self.prev_y[x, y]
+            x, y = self.forward_x[x, y], self.forward_y[x, y]
         return path
 
 
 @boundscheck(False)
 @wraparound(False)
 cpdef DijkstraOutput cy_dijkstra(
-    DTYPE_t[:, :] cost,
+    double[:, :] cost,
     Py_ssize_t[:, :] targets,
+    bool checks_enabled = True,
 ):
-    """Run Dijkstras algorithm on a grid, precalulating paths from all points to one of the targets.
+    """
+    
+    Run Dijkstras algorithm on a grid, yielding many-target-shortest paths for each position.
     
     Parameters
     ----------
     cost :
-        Cost array of shape (n, m). Entries must be positive. Use infinity to mark unpathable cells.
+        Cost grid. Entries must be positive. Use infinity to mark unpathable cells.
     targets :
-        Target array of shape (k, 2) containing x and y coordinates of the target points.
+        Target array of shape (*, 2) containing x and y coordinates of the target points.
+    checks_enabled :
+        Pass False to deactivate grid value and target coordinates checks. Defaults to True.
         
     Returns
     -------
     DijkstraOutput :
-        Pathfinding object containing pointer arrays used to construct paths.
+        Pathfinding object containing containing distance and forward pointer grids.
         
     """
 
     cdef:
-        DTYPE_t _sqrt2 = np.sqrt(2)
-        Py_ssize_t[8] neighbours_x = [-1, 1, 0, 0, -1, 1, -1, 1]
-        Py_ssize_t[8] neighbours_y = [0, 0, -1, 1, -1, -1, 1, 1]
-        DTYPE_t[8] neighbours_d = [1, 1, 1, 1, _sqrt2, _sqrt2, _sqrt2, _sqrt2]
-        DTYPE_t[:, :] dist = np.full_like(cost, np.inf)
-        DTYPE_t[:, :] cost_padded = np.pad(cost, 1, "constant", constant_values=np.inf)
-        Py_ssize_t[:, :] prev_x = np.full_like(cost, -1, np.intp)
-        Py_ssize_t[:, :] prev_y = np.full_like(cost, -1, np.intp)
         cpp_pq q = cpp_pq(compare_element)
+        Item u
         Py_ssize_t x, y, x2, y2
-        DTYPE_t alternative
-        pair[double, pair[int, int]] u, v
+        double alternative
+        double[:, :] cost_padded = np.pad(cost, 1, "constant", constant_values=np.inf)
+        double[:, :] distance = np.full_like(cost, np.inf)
+        Py_ssize_t[:, :] forward_x = np.full_like(cost, -1, np.intp)
+        Py_ssize_t[:, :] forward_y = np.full_like(cost, -1, np.intp)
 
-    if np.any(np.less_equal(cost, 0.0)):
-        raise Exception("invalid cost: entries must be strictly positive")
+    if checks_enabled:
+        if np.any(np.less_equal(cost, 0.0)):
+            raise Exception("invalid double: entries must be strictly positive")
 
-    if (
-        np.less(targets, 0).any()
-        or np.greater_equal(targets[:, 0], cost.shape[0]).any()
-        or np.greater_equal(targets[:, 1], cost.shape[1]).any()
-    ):
-        raise Exception("Target out of bounds")
+        if any((
+            np.less(targets, 0).any(),
+            np.greater_equal(targets[:, 0], cost.shape[0]).any(),
+            np.greater_equal(targets[:, 1], cost.shape[1]).any(),
+        )):
+            raise Exception(f"Target out of bounds")
 
+    # initialize queue with targets
     for i in range(targets.shape[0]):
         x = targets[i, 0]
         y = targets[i, 1]
         c = cost[x, y]
         u = (c, (x, y))
         q.push(u)
-        dist[x, y] = c
+        distance[x, y] = c
 
     while not q.empty():
         u = q.top()
@@ -126,14 +140,14 @@ cpdef DijkstraOutput cy_dijkstra(
         x = u.second.first
         y = u.second.second
         for k in range(8):
-            x2 = x + neighbours_x[k]
-            y2 = y + neighbours_y[k]
-            alternative = dist[x, y] + neighbours_d[k] * cost_padded[x2+1, y2+1]
-            if alternative < dist[x2, y2]:
-                dist[x2, y2] = alternative
-                prev_x[x2, y2] = x
-                prev_y[x2, y2] = y
-                v = (alternative, (x2, y2))
-                q.push(v)
+            x2 = x + NEIGHBOURS_X[k]
+            y2 = y + NEIGHBOURS_Y[k]
+            alternative = distance[x, y] + NEIGHBOURS_D[k] * cost_padded[x2+1, y2+1]
+            if alternative < distance[x2, y2]:
+                distance[x2, y2] = alternative
+                forward_x[x2, y2] = x
+                forward_y[x2, y2] = y
+                u = (alternative, (x2, y2))
+                q.push(u)
 
-    return DijkstraOutput(prev_x, prev_y, dist)
+    return DijkstraOutput(forward_x, forward_y, distance)
