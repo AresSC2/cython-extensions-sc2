0ce36d5f053ef7cb0e52f5921133bce042296b0b
Volker Schmidt <volker.schmidt91@gmail.com>
2025-04-05 01:27:18 +0200

    fix: optimize and unroll

--- cython_extensions/dijkstra.pyx
+++ cython_extensions/dijkstra.pyx
@@ -2,7 +2,9 @@ from cython import boundscheck, wraparound
 import numpy as np
 cimport numpy as cnp
 
-from libc.stdlib cimport malloc, free
+from libc.stdlib cimport malloc
+
+DEF HEAP_ARITY = 4
 
 ctypedef cnp.float64_t DTYPE_t
 
@@ -11,70 +13,10 @@ cdef Py_ssize_t[8] NEIGHBOURS_Y = [0, 0, -1, 1, -1, -1, 1, 1]
 cdef DTYPE_t SQRT2 = np.sqrt(2)
 cdef DTYPE_t[8] NEIGHBOURS_D = [1, 1, 1, 1, SQRT2, SQRT2, SQRT2, SQRT2]
 
+
 cdef struct PriorityQueueItem:
     Py_ssize_t x, y
     DTYPE_t distance
-
-cdef class PriorityQueue:
-    cdef:
-        PriorityQueueItem* heap
-        Py_ssize_t capacity, size
-
-    def __cinit__(self, Py_ssize_t capacity):
-        self.heap = <PriorityQueueItem*>malloc(capacity * sizeof(PriorityQueueItem))
-        self.capacity = capacity
-        self.size = 0
-
-    cdef free(self):
-        free(self.heap)
-
-    cdef enqueue(self, PriorityQueueItem item):
-        cdef Py_ssize_t index
-        index = self.size
-        self.size += 1
-        self.heap[index] = item
-        self.decrease_key(index)
-
-
-    cdef decrease_key(self, Py_ssize_t index):
-        Py_ssize_t previous
-        PriorityQueueItem parent_item
-        while index != 0:
-            parent = (index - 1) // 2
-            parent_item = self.heap[parent]
-            if item.distance < parent_item.distance:
-                self.heap[index], self.heap[parent] = parent_item, item
-                index, item = parent, parent_item
-            else:
-                break
-
-    cdef PriorityQueueItem dequeue(self):
-        cdef:
-            PriorityQueueItem root = self.heap[0]
-        self.size -= 1
-        self.heap[0] = self.heap[self.size]
-        self.heapify(0)
-        return root
-
-    cdef heapify(self, Py_ssize_t index):
-        cdef Py_ssize_t left, right, swap
-        while True:
-
-            left = 2 * index + 1
-            if left < self.size and self.heap[left].distance < self.heap[index].distance:
-                swap = left
-            else:
-                swap = index
-
-            right = left + 1
-            if right < self.size and self.heap[right].distance < self.heap[swap].distance:
-                swap = right
-
-            if swap != index:
-                self.heap[index], self.heap[swap] = self.heap[swap], self.heap[index]
-                index = swap
-            else:
-                break
 
 
 cdef class DijkstraOutput:
@@ -96,21 +38,21 @@ cdef class DijkstraOutput:
     @wraparound(False)
     cpdef get_path(self, (int, int) source, int limit=0):
         """
-        
+
         Follow the path from a given source using the forward pointer grids.
-        
+
         Parameters
         ----------
         source :
             Start point.
         limit :
             Maximum length of the returned path. Defaults to 0 indicating no limit.
-            
+
         Returns
         -------
         list[tuple[int, int]] :
             The lowest cost path from source to any of the targets.
-            
+
         """
         path = list[tuple[int, int]]()
         x, y = source
@@ -135,9 +77,9 @@ cpdef DijkstraOutput cy_dijkstra(
     bint checks_enabled = True,
 ):
     """
-    
+
     Run Dijkstras algorithm on a grid, yielding many-target-shortest paths for each position.
-    
+
     Parameters
     ----------
     cost :
@@ -146,19 +88,23 @@ cpdef DijkstraOutput cy_dijkstra(
         Target array of shape (*, 2) containing x and y coordinates of the target points.
     checks_enabled :
         Pass False to deactivate grid value and target coordinates checks. Defaults to True.
-        
+
     Returns
     -------
     DijkstraOutput :
         Pathfinding object containing containing distance and forward pointer grids.
-        
+
     """
 
     cdef:
-        PriorityQueue pq = PriorityQueue(cost.shape[0] * cost.shape[1])
+        PriorityQueueItem* pq
+        PriorityQueueItem root
+        Py_ssize_t i, swap, index, parent
+
         PriorityQueueItem u
+        Py_ssize_t capacity, size
         Py_ssize_t x, y, x2, y2
-        DTYPE_t alternative
+        DTYPE_t c, alternative
         DTYPE_t[:, :] cost_padded = np.pad(cost, 1, "constant", constant_values=np.inf)
         DTYPE_t[:, :] distance = np.full_like(cost, np.inf)
         Py_ssize_t[:, :] forward_x = np.full_like(cost, -1, np.intp)
@@ -175,19 +121,51 @@ cpdef DijkstraOutput cy_dijkstra(
         )):
             raise Exception(f"Target out of bounds")
 
+    capacity = cost.size
+    heap = <PriorityQueueItem*>malloc(capacity * sizeof(PriorityQueueItem))
+    size = targets.shape[0]
+
     # initialize queue with targets
     for i in range(targets.shape[0]):
+
+        # add to heap
         x = targets[i, 0]
         y = targets[i, 1]
         c = cost[x, y]
-        u = PriorityQueueItem(x, y, c)
-        pq.enqueue(u)
+        heap[i] = PriorityQueueItem(x, y, c)
         distance[x, y] = c
 
-    while pq.size != 0:
-        u = pq.dequeue()
-        x = u.x
-        y = u.y
+        # heapify
+        index = i
+        while index != 0:
+            parent = (index - 1) // HEAP_ARITY
+            if heap[index].distance < heap[parent].distance:
+                heap[index], heap[parent] = heap[parent], heap[index]
+                index = parent
+            else:
+                break
+
+    while size != 0:
+
+        x = heap[0].x
+        y = heap[0].y
+
+        # dequeue
+        size -= 1
+        heap[0] = heap[size]
+        index = 0
+        while True:
+            swap = index
+            i = HEAP_ARITY * index + 1
+            for child in range(i, i + min(HEAP_ARITY, size - i)):
+                if heap[child].distance < heap[swap].distance:
+                    swap = child
+            if swap != index:
+                heap[index], heap[swap] = heap[swap], heap[index]
+                index = swap
+            else:
+                break
+
         for k in range(8):
             x2 = x + NEIGHBOURS_X[k]
             y2 = y + NEIGHBOURS_Y[k]
@@ -196,7 +174,17 @@ cpdef DijkstraOutput cy_dijkstra(
                 distance[x2, y2] = alternative
                 forward_x[x2, y2] = x
                 forward_y[x2, y2] = y
-                u = PriorityQueueItem(x2, y2, alternative)
-                pq.enqueue(u)
+
+                # enqueue
+                index = size
+                size += 1
+                heap[index] = PriorityQueueItem(x2, y2, alternative)
+                while index != 0:
+                    parent = (index - 1) // HEAP_ARITY
+                    if heap[index].distance < heap[parent].distance:
+                        heap[index], heap[parent] = heap[parent], heap[index]
+                        index = parent
+                    else:
+                        break
 
     return DijkstraOutput(forward_x, forward_y, distance)
